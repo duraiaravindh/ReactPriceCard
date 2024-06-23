1bf3d44fe9526d99fbb726c5e39b783578832bd9
Durai Aravindhraj Elangovan <83654766+duraiaravindh@users.noreply.github.com>
2024-06-23 22:47:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ReactPricingCard from './items/ReactPricingCard';
+
+function App() {
+  return (
+    <div className="pricing py-5">
+      {/* <PricingCard /> */}
+      <ReactPricingCard />
+
+    </div>
+  );
+}
+
+export default App;
